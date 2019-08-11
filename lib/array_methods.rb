8b4979a2f3232ad9f4dbf80dd2620b67297a674e
Mercedes Delin <mercedesgdelin@gmail.com>
2019-08-11 23:39:06 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,9 @@ def find_element_index(array, value_to_find)
   
   array.length.times do |index|
     if array[index] = "#{value_to_find}"
-      return index
+      return [index]
+    else 
+      return NIL
   end
 end
 
