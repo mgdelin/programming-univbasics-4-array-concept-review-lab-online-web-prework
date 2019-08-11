e4bb7f7988175cf9856c6be38f52d5483742d40f
Mercedes Delin <mercedesgdelin@gmail.com>
2019-08-11 23:01:42 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,5 @@
 def find_element_index(array, value_to_find)
-counter = 0
-until array[counter] = "#{value_to_find}" do
-      puts NIL
-  return array[counter]="#{value_to_find}"
-end
-  counter += 1
+array.length.times
 end
 
 def find_max_value(array)
