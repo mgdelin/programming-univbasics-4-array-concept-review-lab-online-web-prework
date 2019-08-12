f7b46ce492fb190309da1d452c47d1d0d8bd54b5
Mercedes Delin <mercedesgdelin@gmail.com>
2019-08-12 00:55:41 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,7 +8,11 @@ end
 
 
 def find_max_value(array)
-  
+  counter = 0
+  while counter < array.length do
+   return counter if array[counter] > 
+   counter += 1
+  end
 end
 
 def find_min_value(array)
