de63fdf944afe6c0d3c870f2bd1a4f652a30252a
Mercedes Delin <mercedesgdelin@gmail.com>
2019-08-12 00:00:00 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,16 +6,12 @@ def find_element_index(array, value_to_find)
       return 
     else 
       return NIL
-      
-    end
-  
-  
-end
+      end
+  end
 
 
 def find_max_value(array)
-  array.max_by do |element|
-    element.field
+  
 end
 
 def find_min_value(array)
