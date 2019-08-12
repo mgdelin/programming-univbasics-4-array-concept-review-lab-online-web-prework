9aedd60ee8cb8f7d8b92c975c74725a181696adb
Mercedes Delin <mercedesgdelin@gmail.com>
2019-08-12 00:03:41 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,9 +2,9 @@ def find_element_index(array, value_to_find)
   counter = 0
   array.length.times 
   if array[counter] = "#{value_to_find}"
-    counter += 1
+    
       return counter
-      
+      counter += 1
       end
   end
 
